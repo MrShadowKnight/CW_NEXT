a0d2de881c4954a11b039d931685287a66c240bc
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-11-07 19:57:26 +0200

    Add.changes

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,11 +1,108 @@
 import React from "react";
-import s from "./header.module.css";
+import Link from "next/link";
+import Image from "next/image";
+// import image
+import imgHeart from "../../assets/img/Heart.png";
+import imgBuy from "../../assets/img/Buy.png";
+// import styles
+import s from "./header.module.scss";
 
 const Header = () => {
   return (
     <>
       <header className={s.header}>
-        <h1>Header</h1>
+        <h1>Logo</h1>
+        <div className={s.header__search}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21"
+            fill="none"
+          >
+            <ellipse
+              cx="9.86616"
+              cy="9.76663"
+              rx="9.01674"
+              ry="8.98856"
+              stroke="#200E32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M16.1375 16.4851L19.6725 20"
+              stroke="#200E32"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <input type="text" />
+        </div>
+        <nav>
+          <ul className={s.header__menu}>
+            <li>
+              <Link href="#">
+                Товари
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="10"
+                  viewBox="0 0 16 10"
+                  fill="none"
+                >
+                  <path
+                    d="M15 1.5L8 8.5L1 1.5"
+                    stroke="#200E32"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">Про нас</Link>
+            </li>
+            <li>
+              <Link href="#">Контакти</Link>
+            </li>
+            <li>
+              <Link href="#">
+                UA
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="10"
+                  viewBox="0 0 16 10"
+                  fill="none"
+                >
+                  <path
+                    d="M15 1.5L8 8.5L1 1.5"
+                    stroke="#200E32"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <Image src={imgHeart} width={18} height={18} alt=""></Image>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <Image src={imgBuy} width={18} height={18} alt=""></Image>
+              </Link>
+            </li>
+            <li>
+              <button>Вхід</button>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
